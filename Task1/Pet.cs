7cea419fedf426c02be75c9b2ba34ab3140fcc1e
Вячеслав <slavvva05@gmail.com>
2024-06-15 22:04:25 +0300

    Добавьте файлы проекта.

---
+++ Task1/Pet.cs
@@ -0,0 +1,110 @@
+﻿namespace Task1
+{
+    internal class Pet
+    {
+        private string _name;
+        private string _breed;
+        private int _age;
+
+        #region Констуркторы
+        public Pet() : this("Неизвестно", "Неизвестно", 0)
+        {
+        }
+
+        public Pet(string name, string breed, int age)
+        {
+            _name = name;
+            _breed = breed;
+            _age = age;
+        }
+        #endregion
+
+        #region Методы
+        public void Print()
+        {
+            Console.WriteLine($"Кличка: {Name} \nПорода: {Breed} \nВозраст: {Age} \n");
+        }
+        #endregion
+
+        #region Перегрузки операторов
+        public static Pet operator ++(Pet pet) => new Pet
+        {
+            Name = pet.Name,
+            Breed = pet.Breed,
+            Age = pet.Age + 1
+        };
+
+        public static Pet operator +(Pet pet1, Pet pet2) => new Pet
+        {
+            Name = pet1.Name,
+            Breed = pet1.Breed,
+            Age = pet1.Age + pet2.Age
+        };
+
+        public static bool operator ==(Pet pet1, Pet pet2) => (pet1.Name == pet2.Name && pet1.Breed == pet2.Breed && pet1.Age == pet2.Age);
+
+        public static bool operator !=(Pet pet1, Pet pet2) => (pet1.Name != pet2.Name || pet1.Breed != pet2.Breed || pet1.Age != pet2.Age);
+
+        public static bool operator true(Pet pet) => (pet.Age >= 0);
+
+        public static bool operator false(Pet pet) => (pet.Age < 0);
+        #endregion
+
+        #region Индексаторы
+        public object? this[string index]
+        {
+            get
+            {
+                return index switch
+                {
+                    "Кличка" => _name,
+                    "Порода" => _breed,
+                    _ => null
+                };
+            }
+        }
+
+        public char this[int index]
+        {
+            get
+            {
+                if (index <= Name.Length && index >= 0)
+                    return Name[index];
+                return '\0';
+            }
+        }
+        #endregion
+
+        #region Свойства
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                value = _name.Trim();
+                if (_name != value && value != "")
+                    _name = value;
+            }
+        }
+        public string Breed
+        {
+            get => _breed;
+            set
+            {
+                value = _breed.Trim();
+                if (_breed != value && value != "")
+                    _breed = value;
+            }
+        }
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                if (_age >= 0)
+                    _age = value;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel.Design;
+using Task1;
+
+Pet pet1= new("Васька", "Капибара", 18);
+Pet pet2 = new("Петя", "Кот", 20);
+Pet pet4 = new("Петя", "Кот", 20);
+Pet pet5 = new("Жора", "Собака", -10);
+
+#region Task1
+Console.WriteLine("----------------Task 1----------------");
+
+pet1.Print();
+
+pet1++;
+pet1.Print();
+#endregion
+
+#region Task2
+Console.WriteLine("----------------Task 2----------------");
+
+Console.WriteLine("pet1");
+pet1.Print();
+Console.WriteLine("pet2");
+pet2.Print();
+
+Pet pet3 = pet1 + pet2;
+Console.WriteLine("pet3");
+pet3.Print();
+#endregion
+
+#region Task3
+Console.WriteLine("----------------Task 3----------------");
+
+Console.WriteLine("pet1");
+pet1.Print();
+Console.WriteLine("pet2");
+pet2.Print();
+Console.WriteLine("pet4");
+pet4.Print();
+
+if (pet1 == pet2)
+    Console.WriteLine("pet1 и pet2 одинаковые");
+else
+    Console.WriteLine("pet1 и pet2 разные");
+
+if (pet2 == pet4)
+    Console.WriteLine("pet2 и pet4 одинаковые");
+else
+    Console.WriteLine("pet2 и pet4 разные");
+#endregion
+
+#region Task4
+Console.WriteLine("----------------Task 4----------------");
+
+Console.WriteLine("pet1");
+pet1.Print();
+Console.WriteLine("pet5");
+pet5.Print();
+
+if (pet1)
+    Console.WriteLine("Возраст pet1 >= 0 ");
+else
+    Console.WriteLine("Возраст pet1 < 0 ");
+
+if (pet5)
+    Console.WriteLine("Возраст pet5 >= 0 ");
+else
+    Console.WriteLine("Возраст pet5 < 0");
+#endregion
